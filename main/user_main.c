7ef46c5bcb5755f737b5f64eebcc434a0e53ae28
Vespertilio Sh <hellvesper@gmail.com>
2022-07-09 13:08:57 +0300

    mqtt sample

--- main/user_main.c
+++ main/user_main.c
@@ -10,12 +10,15 @@
 
 
 #include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
 #include <string.h>
 #include <stdlib.h>
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
+#include "freertos/semphr.h"
 
 #include "esp_log.h"
 #include "esp_system.h"
@@ -24,7 +27,22 @@
 #include "driver/i2c.h"
 
 
-static const char *TAG = "main";
+static const char *I2C_TAG = "i2c";
+
+#include "esp_wifi.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
+
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+
+#include "mqtt_client.h"
+#include "mqtt_cred.h"
+
+static const char *MQTT_TAG = "MQTT_EXAMPLE";
 
 /**
  * TEST CODE BRIEF
@@ -59,8 +77,7 @@ static const char *TAG = "main";
 #define I2C_EXAMPLE_MASTER_RX_BUF_DISABLE   0                /*!< I2C master do not need buffer */
 
 #define MPU6050_SENSOR_ADDR                 0x48             /*!< slave address for MPU6050 sensor */
-#define MPU6050_CMD_START                   0x41             /*!< Command to set measure mode */
-#define MPU6050_WHO_AM_I                    0x75             /*!< Command to read WHO_AM_I reg */
+
 #define WRITE_BIT                           I2C_MASTER_WRITE /*!< I2C master write */
 #define READ_BIT                            I2C_MASTER_READ  /*!< I2C master read */
 #define ACK_CHECK_EN                        0x1              /*!< I2C master will check ack from slave*/
@@ -86,6 +103,11 @@ static const char *TAG = "main";
 #define CMD_SLAVE_GET_SENSORS   0xF0
 #define CMD_TYPE_2_SLAVE        2
 #define PING                    0xA0
+
+/**
+ * MQTT DEFINES
+ */
+#define MQTT_BROKER_URL     MQTT_URI
 
 #define BUFFER_SIZE      20  // 20bytes
 #define CHANNELS         8
@@ -215,7 +237,7 @@ i2c_example_master_mpu6050_read(i2c_port_t i2c_num, uint8_t reg_address, uint8_t
 }
 
 static esp_err_t i2c_example_master_mpu6050_init(i2c_port_t i2c_num) {
-    uint8_t cmd_data;
+//    uint8_t cmd_data;
     vTaskDelay(100 / portTICK_RATE_MS);
     i2c_example_master_init();
 //    cmd_data = 0x00;    // reset mpu6050
@@ -252,19 +274,19 @@ static void i2c_task_example(void *arg) {
         ret = i2c_example_master_mpu6050_read(I2C_EXAMPLE_MASTER_NUM, ACCEL_XOUT_H, sensor_data, 14);
 
         if (ret == ESP_OK) {
-            ESP_LOGI(TAG, "*******************\n");
-            ESP_LOGI(TAG, "WHO_AM_I: 0x%02x\n", who_am_i);
+            ESP_LOGI(I2C_TAG, "*******************\n");
+            ESP_LOGI(I2C_TAG, "WHO_AM_I: 0x%02x\n", who_am_i);
             Temp = 36.53 + ((double) (int16_t) ((sensor_data[6] << 8) | sensor_data[7]) / 340);
-            ESP_LOGI(TAG, "TEMP: %d.%d\n", (uint16_t) Temp, (uint16_t) (Temp * 100) % 100);
+            ESP_LOGI(I2C_TAG, "TEMP: %d.%d\n", (uint16_t) Temp, (uint16_t) (Temp * 100) % 100);
 
             for (i = 0; i < 7; i++) {
-                ESP_LOGI(TAG, "sensor_data[%d]: %d\n", i,
+                ESP_LOGI(I2C_TAG, "sensor_data[%d]: %d\n", i,
                          (int16_t) ((sensor_data[i * 2] << 8) | sensor_data[i * 2 + 1]));
             }
 
-            ESP_LOGI(TAG, "error_count: %d\n", error_count);
+            ESP_LOGI(I2C_TAG, "error_count: %d\n", error_count);
         } else {
-            ESP_LOGE(TAG, "No ack, sensor not connected...skip...\n");
+            ESP_LOGE(I2C_TAG, "No ack, sensor not connected...skip...\n");
         }
 
         vTaskDelay(100 / portTICK_RATE_MS);
@@ -274,7 +296,7 @@ static void i2c_task_example(void *arg) {
 }
 
 static void i2c_task_ping(void *arg) {
-    uint8_t pong, i;
+    uint8_t pong;
     static uint32_t error_count = 0;
     int ret;
     UBaseType_t uxHighWaterMark;
@@ -282,7 +304,7 @@ static void i2c_task_ping(void *arg) {
 
     ret = i2c_example_master_mpu6050_init(I2C_EXAMPLE_MASTER_NUM);
     if (ret != ESP_OK)
-        ESP_LOGE(TAG, "I2C Init ERROR \n");
+        ESP_LOGE(I2C_TAG, "I2C Init ERROR \n");
 
     while (1) {
         pong = 0;
@@ -293,10 +315,10 @@ static void i2c_task_ping(void *arg) {
         }
 
         if (ret == ESP_OK) {
-            ESP_LOGI(TAG, "*******************\n");
-            ESP_LOGI(TAG, "PONG: 0x%02x\n", pong);
-            ESP_LOGI(TAG, "error_count: %d\n", error_count);
-            ESP_LOGD(TAG, "uxHighWaterMark: %d\n", uxHighWaterMark);
+            ESP_LOGI(I2C_TAG, "*******************\n");
+            ESP_LOGI(I2C_TAG, "PONG: 0x%02x\n", pong);
+            ESP_LOGI(I2C_TAG, "error_count: %d\n", error_count);
+            ESP_LOGD(I2C_TAG, "uxHighWaterMark: %d\n", uxHighWaterMark);
 
             if (pong == 0xAA) {
                 if (I2C_1S_Task_Handler != NULL)
@@ -304,7 +326,7 @@ static void i2c_task_ping(void *arg) {
 //                if (I2C_1S_Task_Handler != NULL)
 //                    xTaskNotifyGive(I2C_1S_Task_Handler);
             } else {
-                ESP_LOGE(TAG, "PONG request wrong, reset MSP..\n");
+                ESP_LOGE(I2C_TAG, "PONG request wrong, reset MSP..\n");
 //                vTaskDelay(100 / portTICK_RATE_MS);
 //                gpio_set_level(GPIO_MSP_RST, 0);  // RST MSP
 //                vTaskDelay(500 / portTICK_RATE_MS); // wait till capacitor discharge
@@ -312,13 +334,13 @@ static void i2c_task_ping(void *arg) {
 //                vTaskDelay(100 / portTICK_RATE_MS); // extra delay for MSP boot process
             }
         } else {
-            ESP_LOGE(TAG, "No ack, sensor not connected...restarting i2c driver...\n");
-            ESP_LOGD(TAG, "uxHighWaterMark: %d\n", uxHighWaterMark);
+            ESP_LOGE(I2C_TAG, "No ack, sensor not connected...restarting i2c driver...\n");
+            ESP_LOGD(I2C_TAG, "uxHighWaterMark: %d\n", uxHighWaterMark);
             ret = i2c_driver_delete(I2C_EXAMPLE_MASTER_NUM);
             if (ret == ESP_OK) {
                 i2c_example_master_mpu6050_init(I2C_EXAMPLE_MASTER_NUM);
             } else
-                ESP_LOGE(TAG, "Can't delete driver.\n");
+                ESP_LOGE(I2C_TAG, "Can't delete driver.\n");
             vTaskDelay(100 / portTICK_RATE_MS);
         }
 
@@ -332,8 +354,8 @@ static void i2c_task_ping(void *arg) {
 static void i2c_task_ticks_ms(void *arg) {
     uint8_t data_buffer[BUFFER_SIZE];
     uint16_t data[CHANNELS];
-    uint8_t i;
-    static uint32_t error_count = 0;
+//    uint8_t i;
+//    static uint32_t error_count = 0;
     int ret;
     UBaseType_t uxHighWaterMark;
     uxHighWaterMark = uxTaskGetStackHighWaterMark(NULL);
@@ -349,16 +371,16 @@ static void i2c_task_ticks_ms(void *arg) {
                 data[j] = data_buffer[j + j] << 8;
                 data[j] += data_buffer[j + j + 1];
             }
-//            ESP_LOGI(TAG, "*******************\n");
-//            ESP_LOGI(TAG, "PONG: 0x%02x\n", pong);
-            ESP_LOGI(TAG, "TICKS20MS #1: %d | #2: %d | #3: %d | #4: %d | #5: %d | #6: %d | #7: %d | #8: %d",
+//            ESP_LOGI(I2C_TAG, "*******************\n");
+//            ESP_LOGI(I2C_TAG, "PONG: 0x%02x\n", pong);
+            ESP_LOGI(I2C_TAG, "TICKS20MS #1: %d | #2: %d | #3: %d | #4: %d | #5: %d | #6: %d | #7: %d | #8: %d",
                      data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]);
         } else {
-            ESP_LOGE(TAG, "No ack, %d\n", ret);
+            ESP_LOGE(I2C_TAG, "No ack, %d\n", ret);
         }
-        ESP_LOGD(TAG, "uxHighWaterMark: %d\n", uxHighWaterMark);
-        ESP_LOGD(TAG, "MinFreeHeap: %d\n", esp_get_minimum_free_heap_size());
-        ESP_LOGD(TAG, "FreeHeap: %d\n", esp_get_free_heap_size());
+        ESP_LOGD(I2C_TAG, "uxHighWaterMark: %d\n", uxHighWaterMark);
+        ESP_LOGD(I2C_TAG, "MinFreeHeap: %d\n", esp_get_minimum_free_heap_size());
+        ESP_LOGD(I2C_TAG, "FreeHeap: %d\n", esp_get_free_heap_size());
 
         uxHighWaterMark = uxTaskGetStackHighWaterMark(NULL);
         vTaskDelay(1000 / portTICK_RATE_MS);
@@ -370,8 +392,7 @@ static void i2c_task_ticks_ms(void *arg) {
 static void i2c_task_ticks_sec(void *arg) {
     uint8_t data_buffer[BUFFER_SIZE];
     uint16_t data[CHANNELS];
-    uint8_t i;
-    static uint32_t error_count = 0;
+//    static uint32_t error_count = 0;
     int ret;
     UBaseType_t uxHighWaterMark;
     uxHighWaterMark = uxTaskGetStackHighWaterMark(NULL);
@@ -387,22 +408,86 @@ static void i2c_task_ticks_sec(void *arg) {
                 data[j] = data_buffer[j + j] << 8;
                 data[j] += data_buffer[j + j + 1];
             }
-//            ESP_LOGI(TAG, "*******************\n");
-//            ESP_LOGI(TAG, "PONG: 0x%02x\n", pong);
-            ESP_LOGI(TAG, "TICKS1SEC #1: %d | #2: %d | #3: %d | #4: %d | #5: %d | #6: %d | #7: %d | #8: %d",
+//            ESP_LOGI(I2C_TAG, "*******************\n");
+//            ESP_LOGI(I2C_TAG, "PONG: 0x%02x\n", pong);
+            ESP_LOGI(I2C_TAG, "TICKS1SEC #1: %d | #2: %d | #3: %d | #4: %d | #5: %d | #6: %d | #7: %d | #8: %d",
                      data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]);
         } else {
-            ESP_LOGE(TAG, "No ack, sensor not connected...skip...\n");
+            ESP_LOGE(I2C_TAG, "No ack, sensor not connected...skip...\n");
         }
-        ESP_LOGD(TAG, "uxHighWaterMark: %d\n", uxHighWaterMark);
-        ESP_LOGD(TAG, "MinFreeHeap: %d\n", esp_get_minimum_free_heap_size());
-        ESP_LOGD(TAG, "FreeHeap: %d\n", esp_get_free_heap_size());
+        ESP_LOGD(I2C_TAG, "uxHighWaterMark: %d\n", uxHighWaterMark);
+        ESP_LOGD(I2C_TAG, "MinFreeHeap: %d\n", esp_get_minimum_free_heap_size());
+        ESP_LOGD(I2C_TAG, "FreeHeap: %d\n", esp_get_free_heap_size());
 
         vTaskDelay(1000 / portTICK_RATE_MS);
         uxHighWaterMark = uxTaskGetStackHighWaterMark(NULL);
     }
 
     i2c_driver_delete(I2C_EXAMPLE_MASTER_NUM);
+}
+
+static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event) {
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+    // your_context_t *context = event->context;
+    switch (event->event_id) {
+        case MQTT_EVENT_CONNECTED:
+            ESP_LOGI(MQTT_TAG, "MQTT_EVENT_CONNECTED");
+            msg_id = esp_mqtt_client_publish(client, "/topic/qos1", "data_3", 0, 1, 0);
+            ESP_LOGI(MQTT_TAG, "sent publish successful, msg_id=%d", msg_id);
+
+            msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
+            ESP_LOGI(MQTT_TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+            msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
+            ESP_LOGI(MQTT_TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+            msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
+            ESP_LOGI(MQTT_TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
+            break;
+        case MQTT_EVENT_DISCONNECTED:
+            ESP_LOGI(MQTT_TAG, "MQTT_EVENT_DISCONNECTED");
+            break;
+
+        case MQTT_EVENT_SUBSCRIBED:
+            ESP_LOGI(MQTT_TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+            msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
+            ESP_LOGI(MQTT_TAG, "sent publish successful, msg_id=%d", msg_id);
+            break;
+        case MQTT_EVENT_UNSUBSCRIBED:
+            ESP_LOGI(MQTT_TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+            break;
+        case MQTT_EVENT_PUBLISHED:
+            ESP_LOGI(MQTT_TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+            break;
+        case MQTT_EVENT_DATA:
+            ESP_LOGI(MQTT_TAG, "MQTT_EVENT_DATA");
+            printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+            printf("DATA=%.*s\r\n", event->data_len, event->data);
+            break;
+        case MQTT_EVENT_ERROR:
+            ESP_LOGI(MQTT_TAG, "MQTT_EVENT_ERROR");
+            break;
+        default:
+            ESP_LOGI(MQTT_TAG, "Other event id:%d", event->event_id);
+            break;
+    }
+    return ESP_OK;
+}
+
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data) {
+    ESP_LOGD(MQTT_TAG, "Event dispatched from event loop base=%s, event_id=%d", base, event_id);
+    mqtt_event_handler_cb(event_data);
+}
+
+static void mqtt_app_start(void) {
+    esp_mqtt_client_config_t mqtt_cfg = {
+            .uri = MQTT_BROKER_URL,
+    };
+
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, client);
+    esp_mqtt_client_start(client);
 }
 
 void app_main(void) {
@@ -435,4 +520,28 @@ void app_main(void) {
     xTaskCreate(i2c_task_ping, "i2c_task_example", 1024, NULL, 11, NULL);
 //    xTaskCreate(i2c_task_ticks_ms, "i2c_task_ticks_ms", 2048, NULL, 10, &I2C_MS_Task_Handler);
     xTaskCreate(i2c_task_ticks_sec, "i2c_task_ticks_sec", 2048, NULL, 10, &I2C_1S_Task_Handler);
+
+    ESP_LOGI(MQTT_TAG, "[APP] Startup..");
+    ESP_LOGI(MQTT_TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
+    ESP_LOGI(MQTT_TAG, "[APP] IDF version: %s", esp_get_idf_version());
+
+    esp_log_level_set("*", ESP_LOG_INFO);
+    esp_log_level_set("MQTT_CLIENT", ESP_LOG_VERBOSE);
+    esp_log_level_set("MQTT_EXAMPLE", ESP_LOG_VERBOSE);
+    esp_log_level_set("TRANSPORT_TCP", ESP_LOG_VERBOSE);
+    esp_log_level_set("TRANSPORT_SSL", ESP_LOG_VERBOSE);
+    esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
+    esp_log_level_set("OUTBOX", ESP_LOG_VERBOSE);
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+
+    mqtt_app_start();
 }
